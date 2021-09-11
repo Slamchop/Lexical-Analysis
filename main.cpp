89bd837ee95cc9c136c68cc175cfbc6089b7952d
Jared Tuck <jared.tuck@sjsu.edu>
2021-09-11 09:57:34 -0700

    Jareds plan for the project, let me know what you think

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main{
-
+        Lexer mylexer = new Lexer();
+        while(true) {
+            Token tok = mylexer.getNextToken();
+            if(tok == null) { // print error message break;   }
+                // else if (tok.tag == EOF) { break;   }
+                // else { // print token name and value
+                // (if your token doesn’t have a name, add logic to print value twice)
+                 }
+             }
         return 0;
-};
+}
